cfdfdfc7ba2d1e2a39e0e494dcc9dfe259443950
jogindrakumar <joginder9868@outlook.com>
2023-02-07 20:05:45 +0530

    comments in cSharp

---
+++ learnCsharp/Comments_In_CSharp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learnCsharp
+{
+    internal class Comments_In_CSharp
+    {
+        //How to comment code in C# 
+        //this is commented line 
+        //or use shortcut like ctrl +k+c
+        //hello comment
+    }
+}
